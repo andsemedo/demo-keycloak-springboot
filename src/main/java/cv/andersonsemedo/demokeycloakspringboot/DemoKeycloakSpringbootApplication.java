cfa1498d9170420446a98a453bdb08c2ad9396df
Anderson Semedo <andsemedo023@gmail.com>
2025-08-05 11:36:42 -0100

    initial commit

---
+++ src/main/java/cv/andersonsemedo/demokeycloakspringboot/DemoKeycloakSpringbootApplication.java
@@ -0,0 +1,13 @@
+package cv.andersonsemedo.demokeycloakspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKeycloakSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoKeycloakSpringbootApplication.class, args);
+    }
+
+}
